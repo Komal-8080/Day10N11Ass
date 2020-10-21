cdb324ff4d0238ce55260c4b39a86b87a64363a2
Komal-8080 <shindeg163@gmail.com>
2020-10-22 01:03:27 +0530

    Values Stored in Dictionary

--- Day10SortingArithmaticComputation.sh
+++ Day10SortingArithmaticComputation.sh
@@ -9,15 +9,35 @@ read b
 echo "Enter Value of c"
 read c
 
+echo " "
+
+declare -A computeArray=()
+
+
 	Computation1=$((a+b*c))
         echo "Value of a+b*c is $Computation1"
+	computeArray+=([a+b*c]=$Computation1)
 
 	Computation2=$((a*b+c))
         echo "Value of a*b+c is $Computation2"
+	computeArray+=([a*b+c]=$Computation2)
+
 
 	Computation3=$((c+a/b))
 	echo "Value of c+a/b is $Computation3"
+	computeArray+=([c+a/b]=$Computation3)
+
 
 	Computation4=$((a%b+c))
 	echo "Value of a%b+c is $Computation4"
+	computeArray+=([a%b+c]=$Computation4)
+
+	echo " "
+	echo "Values in Dictionary are : "
+	for key in "${!computeArray[@]}"
+	do
+        echo "$key => ${computeArray[$key]}"
+	done
+
+
 
